ecc9b1a44c8389f7a3705ee2edc92f0d4f8570d4
cpxhylty <1040659410@qq.com>
2020-02-10 10:30:36 +0800

    use input()

--- removeStartParaSpace.py
+++ removeStartParaSpace.py
@@ -2,17 +2,20 @@ from docx import Document
 from docx.oxml.ns import qn
 import re
 
-docxName = '金梦城1月（6551-6600） .docx'
+docxName = input('input file name: ')
+
+count = 0
 
 document = Document(docxName)
 length = len(document.paragraphs)
 for index in range(length):
     if re.match('\d{4}', document.paragraphs[index].text) is not None:  # 是startpara
-        #print(document.paragraphs[index].text)
+        count += 1
+        print(document.paragraphs[index].text[0:4])
         document.paragraphs[index].text = re.sub('\s(?=[āáǎàōóǒòēéěèīíǐìūúǔùǖǘǚǜüa-z])', '', document.paragraphs[index].text)
         for run in document.paragraphs[index].runs:
             run.font.name = '宋体'
             run._element.rPr.rFonts.set(qn('w:eastAsia'), '宋体')
             run.font.name = 'Times New Roman'
 document.save(docxName)
-print('finish')
+print('finish, words count: {}'.format(count))
